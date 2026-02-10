364a79e0e58f0bbd32f00d46086643d558e7fd55
Ahmed El-Sayed Rabie <125227494+Ahmed543252@users.noreply.github.com>
2026-02-10 17:39:03 +0200

    Time: 2126 ms (14.89%), Space: 473.3 MB (64.57%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            set<int>e,o;
+            for (int j = i ; j < n; j++) {
+                if(nums[j]&1){
+                    o.insert(nums[j]);
+                }
+                else{
+                    e.insert(nums[j]);
+                }
+                if(e.size() == o.size()){
+                    ans =max(ans ,j-i+1);
+                }
+            }
+        }
+        return ans ;
+    }
+};
